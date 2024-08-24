0828fc200493e3710523d3b4e876925934ff1f82
spageektti <155078792+spageektti@users.noreply.github.com>
2024-08-24 21:27:46 +0200

    try implement page content fetching

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+function getPageContent(page_url) {
+    try {
+        return fetch(page_url);
+    } catch {
+        return '';
+    }
+}
+
 const currentUrl = window.location.href;
 const url = new URL(currentUrl);
 const params = new URLSearchParams(url.search);
@@ -13,8 +21,11 @@ if (lang === 'en') {
     converted_lang = '.' + lang;
 }
 
-const path = "pages" + converted_lang + '/' + platform + '/' + page + ".md";
-console.log("Editing file " + path);
+const path = 'pages' + converted_lang + '/' + platform + '/' + page + '.md';
+console.log('Editing file ' + path);
 
 const page_url = 'https://raw.githubusercontent.com/tldr-pages/tldr/main/' + path;
-const page_content = fetch(page_url);
+const page_content = getPageContent(page_url);
+
+const editor = document.getElementById('editor');
+editor.textContent = page_content;
